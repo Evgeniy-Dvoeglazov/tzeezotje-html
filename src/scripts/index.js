a2bbdafd26cc601b595786f9095bea75b410c26d
Evgeniy Dvoeglazov <jindv@yandex.ru>
2023-08-09 18:03:28 +0300

    Настроить адаптивность. Настроить форму обратной связи

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,55 +1,77 @@
-new Swiper('.swiper', {
+const burgerBtn = document.querySelector('.navigation__checkbox');
+const menu = document.querySelector('.header__nav-menu');
+const phone = document.querySelector('.header__phone');
+const headerOverlay = document.querySelector('.header__overlay');
+const reserveBtn = document.querySelector('.intro__button');
+const popup = document.querySelector('.popup');
+const closePopupBtn = document.querySelector('.popup__close-btn');
+
+// Подключение свайпера
+const swiper = new Swiper('.swiper', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 });
 
-$(document).ready(function () {
-  $("form").submit(function (evt) {
-    evt.preventDefault();
+// Обработка отправки формы
+$('.popup__form').submit(function (evt) {
+  evt.preventDefault();
 
-      $.ajax({
-          type: $(this).attr('method'),
-          url: $(this).attr('action'),
-          contentType: false,
-          cache: false,
-          processData: false,
-          data: new FormData(this),
-          success: function (result) {
-              // Вывод текста результата отправки
-              alert(result);
-          },
-          // Вывод статуса ошибки
-          error: function (error) {
-            console.log(error.status);
-          }
-      });
-      return false;
+  $.ajax({
+    type: $(this).attr('method'),
+    url: $(this).attr('action'),
+    contentType: false,
+    cache: false,
+    processData: false,
+    data: new FormData(this),
+    success: function (result) {
+      // Вывод текста результата отправки
+      alert(result);
+      closePopup();
+    },
+    // Вывод статуса ошибки
+    error: function (error) {
+      console.log(error.status);
+      closePopup();
+    }
   });
+  return false;
 });
 
-// $(document).ready(function() {
+// Обработчик открытия и закрытия меню
+function changeMenuVisibility() {
+  menu.classList.toggle('header__nav-menu_opened');
+  phone.classList.toggle('header__phone-menu_opened');
+  headerOverlay.classList.toggle('header__overlay_opened');
+}
 
-//   $('form').submit(function(event) {
+// Открытие формы заявки
+function openPopup() {
+  popup.classList.add('popup_opened');
+  closePopupBtn.addEventListener('click', closePopup);
+}
 
-//       event.preventDefault()
+// Закрытие формы заявки на кнопку закрытия
+function closePopup() {
+  popup.classList.remove('popup_opened');
+  closePopupBtn.removeEventListener('click', closePopup);
+}
 
-//       $.ajax({
-//           url: 'send.php',
-//           method: 'POST',
-//           type: 'POST',
-//           data: new FormData(this),
-//           accepts: {
-//             mycustomtype: 'application/x-some-custom-type'
-//           },
-//           contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-//           processData: false,
-//           success: function(result) {
-//               alert('ок')
-//           }
-//       })
-//       a = new FormData(this)
-//       console.log(a)
-//   })
-// })
+// Слушатели событий
+burgerBtn.addEventListener('click', changeMenuVisibility);
+reserveBtn.addEventListener('click', openPopup);
+
+// Закрытие попапа при нажатии на оверлей
+popup.addEventListener('mousedown', (evt) => {
+  if (evt.target.classList.contains('popup_opened')) {
+    closePopup();
+  }
+});
+
+// Закрытие попапа ESC
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    closePopup();
+  }
+});
